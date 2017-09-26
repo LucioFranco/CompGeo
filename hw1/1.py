5af83649558621785402d24737cd326631d4abe1
samwindham1 <sam.windham95@gmail.com>
2017-09-26 00:55:14 -0600

    Cleaned up, fixed logic,  added testing

--- hw1/1.py
+++ hw1/1.py
@@ -1,67 +1,64 @@
 # HW 1 Probelm 1
-# Sam Windham
-#
-#   Find if polygon is cloackwise.
-#   Uses primitive turn algorithm on every
-#     three points and sums each result.
-#   A negative result means a clockwise polygon,
-#     else, counter-clockwise.
-#   Iterates through all n vertices.
-#     => T(n) = O(n)
+# Sam Windham, Lucio Franco
+# 9/26/17
 
 # Object to hold x/y data for polygons
 class Polygon:
-    def __init__(self, n, x, y):
-        self.n = n
-        self.x = x
-        self.y = y
+    def __init__(self, points):
+        self.n = len(points)
+        self.x = [p[0] for p in points]
+        self.y = [p[1] for p in points]
 
-# helper functions for cross product
-#   and primitive turn algorithm
+# helper function for cross product
 def cross(v1,v2):
     return (v1[0] * v2[1]) - (v2[0] * v1[1])
 
-def turn(p, q, r):
-    v1 = (q[0] - p[0], q[1] - p[1])
-    v2 = (r[0] - p[0], r[1] - p[1])
-    c = cross(v1, v2)
-
-    if(c > 0): return 1
-    elif(c < 0): return -1
-    else: return 0
-
 # Primary algorithm for problem 1a.
-#   Checks is each angle is a left
-#   or right turn and sums the results.
+#   Sums cross products of each two neighboring
+#   edges and checks polarity
 def isClockwise(poly):
     sum = 0
-    for i in range(0, poly.n - 2):
+    for i in range(0, poly.n):
         p = (poly.x[i], poly.y[i])
-        q = (poly.x[i+1], poly.y[i+1])
-        sum += cross(p, q)
+        next = (i + 1) % poly.n ## To account for wrap-around
+        q = (poly.x[next], poly.y[next])
+        if cross(p, q) > 0: sum += 1
+        elif cross(p, q) < 0: sum -=1
 
-    if(sum > 0): return True
-    else: return False
+    # Clockwise is negative
+    return  sum <= 0
 
 # Primary algorithm for problem 1b.
 def hasIntersectingSegments(poly):
     return True
 
 
-# Sample data
-A = (-2,4)
-B = (-2,0)
-C = (0,-2)
-D = (2,0)
-E = (0,2)
-F = (2,4)
-G = (0,6)
+### Tests ###
 
-# New example polygon
-n = 7
-x1 = [A[0],B[0],C[0],D[0],E[0],F[0],G[0]]
-y1 = [A[1],B[1],C[1],D[1],E[1],F[1],G[1]]
-p = Polygon(n, x1, y1)
+# Clockwise (True)
+poly1 = Polygon([(0,0), (0,1), (1,0)])
+assert  isClockwise(poly1) == True
 
-# Test example
-print isClockwise(p)
+poly2 = Polygon([(0,2), (2,3), (3,2), (2,1), (1,2)])
+assert isClockwise(poly2) == True
+
+poly3 = Polygon([(-2.0, 1.0), (-3.0, 4.0), (0.0, 2.0), (0.0, 4.0), (1.0, 2.0), (3.0, 1.0), (-1.0, -1.0), (2.0, -2.0), (-2.0, -3.0), (-1.0, -2.0), (-4.0, -1.0), (1.0, 1.0), (-4.0, 0.0)])
+assert isClockwise(poly3) == True
+
+# Counter-clockwise (False)
+poly4 = Polygon([(0,0), (1,0), (0,1)])
+assert isClockwise(poly4) == False
+
+poly5 = Polygon([(3,1), (2,3), (1,4), (0,1), (1,2)])
+assert isClockwise(poly5) == False
+
+poly6 = Polygon([(3.0, 2.0), (-1.0, 0.0), (1.0, 4.0), (-4.0, -2.0), (0.0, 0.0), (-4.0, -4.0), (3.0, 1.0), (-1.0, -5.0), (5.0, -1.0)])
+assert isClockwise(poly6) == False
+
+# No direction defaults to clockwise (True)
+poly7 = Polygon([(0,0), (0,3), (0,2)])
+assert isClockwise(poly7) == True
+
+# Success!
+print "All Tests Passed!"
+
