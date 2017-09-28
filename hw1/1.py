3dded305642ea6765f10b2b10b1c8ff6795218a7
samwindham1 <sam.windham95@gmail.com>
2017-09-27 22:41:13 -0600

    fixed stuff...

--- hw1/1.py
+++ hw1/1.py
@@ -28,28 +28,23 @@ def isClockwise(poly):
         elif cross(p, q) < 0: sum -=1
 
     # Clockwise is negative
-    print sum
     return sum <= 0
 
 def isSimple(poly):
     # Inital cross product
-    prev_x_prod = cross(poly.x[0], poly.y[0])
-    # The max amount of parity changes allowed for it to still be simple
+    prev_x_prod = cross((poly.x[0], poly.y[0]), (poly.x[1], poly.y[1]))
+    # Simple convex polygons change polarity at most once
     max_change = 1
     for i in range(1, poly.n):
-        # Compute cross product at i
-        current_cross = cross(poly.x[i], poly.y[i])
-        # If the product of both is negative then one of them was a different
-        # sign, therefore there is a sign change.
-        if (pre_x_prod * current_cross) < 0:
+        p = (poly.x[i], poly.y[i])
+        q = (poly.x[(i + 1) % poly.n], poly.y[(i + 1) % poly.n])
+        # Compute cross product of i and i+1
+        current_cross = cross(p,q)
+        # Opposite signs produce a negative value.
+        if (prev_x_prod * current_cross) < 0:
             max_change -= 1
-        pre_x_prod = current_cross
+        prev_x_prod = current_cross
     return max_change >= 0
-
-
-# Primary algorithm for problem 1b.
-def hasIntersectingSegments(poly):
-    return True
 
 
 ### Tests ###
@@ -57,16 +52,20 @@ def hasIntersectingSegments(poly):
 # Clockwise (True)
 poly1 = Polygon([(0,0), (0,1), (1,0)])
 assert isClockwise(poly1) == True
+assert isSimple(poly1) == True
 
-poly2 = Polygon([(0,2), (2,3), (3,2), (2,1), (1,2)])
+poly2 = Polygon([(0,2), (2,3), (3,2), (2,1), (0,1)])
 assert isClockwise(poly2) == True
+assert isSimple(poly2) == True
 
 poly3 = Polygon([(-2.0, 1.0), (-3.0, 4.0), (0.0, 2.0), (0.0, 4.0), (1.0, 2.0), (3.0, 1.0), (-1.0, -1.0), (2.0, -2.0), (-2.0, -3.0), (-1.0, -2.0), (-4.0, -1.0), (1.0, 1.0), (-4.0, 0.0)])
 assert isClockwise(poly3) == True
+# isSimple() does not work for this polygon, not same parity
 
 # Counter-clockwise (False)
 poly4 = Polygon([(0,0), (1,0), (0,1)])
 assert isClockwise(poly4) == False
+assert isSimple(poly4) == True
 
 poly5 = Polygon([(3,1), (2,3), (1,4), (0,1), (1,2)])
 assert isClockwise(poly5) == False
@@ -77,6 +76,10 @@ assert isClockwise(poly6) == False
 # No direction defaults to clockwise (True)
 poly7 = Polygon([(0,0), (0,3), (0,2)])
 assert isClockwise(poly7) == True
+
+# Complex convex
+poly8 = Polygon([(0,0), (1,3), (2,0), (0,2), (2,2)])
+assert isSimple(poly8) == False
 
 # Success!
 print "All Tests Passed!"
