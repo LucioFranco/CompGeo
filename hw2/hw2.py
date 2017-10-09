340c60b36fcc4906413cc8c9e60de8424450e22e
samwindham1 <sam.windham95@gmail.com>
2017-10-09 13:39:22 -0600

    Homework 2 created

---
+++ hw2/hw2.py
@@ -0,0 +1,25 @@
+# Sam Windham and Lucio Franco
+# Homework 2
+# 10/17/17
+
+
+# Help classes/functions ----------------------
+class Polygon:
+    def __init__(self, points):
+        self.n = len(points)
+        self.x, self.y = zip(*points)
+
+# helper function for cross product
+def cross(v1,v2):
+    return (v1[0] * v2[1]) - (v2[0] * v1[1])
+
+# End helpers --------------------------------
+
+
+
+# n = 6, tangents at L(1,3) and R(3,1)
+p = Polygon([(1,2),(1,3),(2,3),(3,2),(3,1),(2,1)])
+
+
+
+
---
+++ hw2/sam-2.md
@@ -0,0 +1,18 @@
+---
+title: Homework 2
+author: Sam Windham *with Lucio Franco*
+date: 10/17/17
+---
+
+# 1.
+
+# 3.
+## a.
+
+## b.
+
+# 4.
+
+# 5.
+
+
