84ed8ebfc7414bb1fe76d24afff280a053db5311
samwindham1 <sam.windham95@gmail.com>
2017-10-10 09:42:51 -0600

    added to the polgyon class

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -7,6 +7,7 @@
 class Polygon:
     def __init__(self, points):
         self.n = len(points)
+        self.points = points
         self.x, self.y = zip(*points)
 
 # helper function for cross product
@@ -16,11 +17,20 @@ def cross(v1,v2):
 # End helpers --------------------------------
 
 
+# 1.
+def left_tan(p, q):
+    for point in p.points:
+        a = 1 + 1
+    return q
+
+
+
 
 # polygon in CCW order, n = 6
 # tangents at L(1,3) and R(3,1)
 p = Polygon([(2,1),(3,1),(3,2),(2,3),(1,3),(1,2)])
 
-
+result = left_tan(p, (0,0))
+print result
 
 
