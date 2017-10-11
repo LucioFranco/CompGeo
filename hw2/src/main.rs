db06ae9c4cd1453c2e8be581acb186af916fe7d4
samwindham1 <sam.windham95@gmail.com>
2017-10-11 13:45:49 -0600

    had to start over on a lot of things...

--- hw2/src/main.rs
+++ hw2/src/main.rs
@@ -1,42 +1,72 @@
 // Sam Windham, Lucio Franco
 mod util;
-use util::util::{cross, Polygon};
+use util::util::{Polygon, is_left, is_right};
 
-fn left_tangent(poly: Polygon, q: (f32, f32)) -> (f32, f32) {
-    let mut sign = false;
-    let mut left_tan = (0., 0.);
+// returns index of left tangenet to point q
+fn left_tangent(n: u64, p: Vec<(f64, f64)>, q: (f64, f64)) -> u64 {
 
-    for i in 0..poly.n {
+    // above = left
+    // below = right
+
+
+    // test if first point is left tangent
+    if is_right(p[0], p[1], q) && is_left(p[(n - 1) as usize], p[0], q) {
+        return 0;
+    }
+    for i in 1..n {
+        // previous point
+        let prev = p[(i - 1) as usize];
         // current point
-        let current = poly.points[i as usize];
-        // next poi nt
-        let next = poly.points[((i + 1) % poly.n) as usize];
+        let current = p[i as usize];
 
-        // edge vector between current and next
-        let edge_vec = (next.0 - current.0, next.1 - current.1);
-        let q_vec = (current.0 - q.0, current.1 - q.1);
+        let left = is_left(prev, current, q);
 
-        let cross = cross(q_vec, edge_vec);
-
-        if sign && (sign != (cross > 0.)) {
-            left_tan = current;
-        }
-
-        sign = cross > 0.;
-
+        println!("{} - ({}, {}): {}", i, current.0, current.1, left);
     }
 
-    left_tan
+    (n - 1) as u64
+
 }
 
 fn main() {
-    let poly = Polygon {
-        n: 3,
-        points: vec![(1.0, 1.0), (3.0, 1.0), (1.0, 3.0)],
-    };
-
+    // q point
     let origin = (0.0, 0.0);
 
-    let result: (f32, f32) = left_tangent(poly, origin);
-    println!("\nLeft Tangent: ({},{})", result.0, result.1);
+    // Left tangent = (1, 3)
+    //let poly_small = Polygon {
+    //    n: 3,
+    //    points: vec![(1.0, 1.0), (3.0, 1.0), (1.0, 3.0)],
+    // };
+    //let mut result: (f64, f64) = left_tangent(poly_small, origin);
+    //let mut correct: (f64, f64) = (1.0, 3.0);
+    //assert!(result.0 - correct.0 <= 0.000000000001 && result.1 - correct.0 <= 0.000000000001);
+
+    // Left tangent = 9:(1.25, 4.7)
+    let poly_large = Polygon {
+        n: 17,
+        points: vec![
+            (6.46, 3.22),
+            (6.46, 4.22),
+            (6.10, 5.15),
+            (5.43, 5.89),
+            (4.53, 6.34),
+            (3.53, 6.43),
+            (2.57, 6.16),
+            (1.77, 5.55),
+            (1.25, 4.70),
+            (1.06, 3.72),
+            (1.25, 2.74),
+            (1.77, 1.89),
+            (2.57, 1.28),
+            (3.53, 1.01),
+            (4.53, 1.10),
+            (5.43, 1.55),
+            (6.10, 2.29),
+        ],
+    };
+    let result: u64 = left_tangent(poly_large.n, poly_large.points, origin);
+    //let correct: (f64, f64) = (1.25, 4.70);
+    println!("({})", result);
+    //assert!(result.0 - correct.0 <= 0.000000000001 && result.1 - correct.0 <= 0.000000000001);
+
 }
--- hw2/src/util.rs
+++ hw2/src/util.rs
@@ -1,10 +1,26 @@
+#![allow(dead_code)]
 pub mod util {
     pub struct Polygon {
-        pub n: u32,
-        pub points: Vec<(f32, f32)>,
+        pub n: u64,
+        pub points: Vec<(f64, f64)>,
     }
 
-    pub fn cross((x1, y1): (f32, f32), (x2, y2): (f32, f32)) -> f32 {
-        return (x1 * y2) - (x2 * y1);
+    pub fn cross((x1, y1): (f64, f64), (x2, y2): (f64, f64)) -> f64 {
+        (x1 * y2) - (x2 * y1)
+    }
+
+    pub fn is_left((p0x, p0y): (f64, f64), (p1x, p1y): (f64, f64), (qx, qy): (f64, f64)) -> bool {
+        // p0 = poly[i]
+        // p1 = poly[i+1]
+        // v1 = p1 - p0
+        // v2 = p0 - q
+        // left = cross(v2, v1) > 0
+        // left = cross((p0 - q),(p1 - p0)) > 0
+
+        cross((p0x - qx, p0y - qy), (p1x - p0x, p1y - p0y)) > 0.
+    }
+
+    pub fn is_right((p0x, p0y): (f64, f64), (p1x, p1y): (f64, f64), (qx, qy): (f64, f64)) -> bool {
+        !is_left((p0x, p0y), (p1x, p1y), (qx, qy))
     }
 }
