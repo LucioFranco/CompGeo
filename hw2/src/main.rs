dfdd5ecf3914ff5060ee0f16265e7dbb8b0ca57f
samwindham1 <sam.windham95@gmail.com>
2017-10-10 22:28:20 -0600

    renamed poly->util, iterative implementation finished

--- hw2/src/main.rs
+++ hw2/src/main.rs
@@ -1,45 +1,35 @@
 // Sam Windham, Lucio Franco
+mod util;
+use util::util::{cross, Polygon};
 
-mod poly;
-use poly::poly::cross;
-use poly::poly::Polygon;
-use poly::poly::Vec2;
-
-fn left_tangent(poly: Polygon, q: Vec2) -> f32 {
+fn left_tangent(poly: Polygon, q: (f32, f32)) -> (f32, f32) {
     let mut sign = false;
+    let mut left_tan = (0., 0.);
 
     for i in 0..poly.n {
         // current point
-        let current: Vec2 = poly.points[i as usize] as Vec2;
-        // next point
-        let next: Vec2 = poly.points[((i + 1) % poly.n) as usize] as Vec2;
+        let current = poly.points[i as usize];
+        // next poi nt
+        let next = poly.points[((i + 1) % poly.n) as usize];
 
         // edge vector between current and next
-        let edge_vec: Vec2 = next - current;
-        let q_vec: Vec2 = current - q;
+        let edge_vec = (next.0 - current.0, next.1 - current.1);
+        let q_vec = (current.0 - q.0, current.1 - q.1);
 
         let cross = cross(q_vec, edge_vec);
 
+        if sign && (sign != (cross > 0.)) {
+            left_tan = current;
+        }
+
         sign = cross > 0.;
 
-        println!(
-            "q({}, {}) x e({}, {}):\t{}",
-            q_vec.0,
-            q_vec.1,
-            edge_vec.0,
-            edge_vec.1,
-            if sign { "left" } else { "right" }
-        );
     }
 
-    0.
+    left_tan
 }
 
 fn main() {
-
-    println!("\nRight-turn: <= 0");
-    println!("Left-turn:  > 0\n");
-
     let poly = Polygon {
         n: 3,
         points: vec![(1.0, 1.0), (3.0, 1.0), (1.0, 3.0)],
@@ -47,6 +37,6 @@ fn main() {
 
     let origin = (0.0, 0.0);
 
-    let result: f32 = left_tangent(poly, origin);
-    println!("\nLeft Tangent: {}", result);
+    let result: (f32, f32) = left_tangent(poly, origin);
+    println!("\nLeft Tangent: ({},{})", result.0, result.1);
 }
---
+++ hw2/src/util.rs
@@ -0,0 +1,10 @@
+pub mod util {
+    pub struct Polygon {
+        pub n: u32,
+        pub points: Vec<(f32, f32)>,
+    }
+
+    pub fn cross((x1, y1): (f32, f32), (x2, y2): (f32, f32)) -> f32 {
+        return (x1 * y2) - (x2 * y1);
+    }
+}
