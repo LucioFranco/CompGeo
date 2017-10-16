ff01196ed3076226a1e06d8a8c7fdadd379e588c
samwindham1 <sam.windham95@gmail.com>
2017-10-16 10:41:59 -0600

    finished and tested. working correctly in O(log n)

--- hw2/src/main.rs
+++ hw2/src/main.rs
@@ -1,58 +1,75 @@
 // Sam Windham, Lucio Franco
 mod util;
-use util::util::{Polygon, is_left, is_right};
+use util::util::{Polygon, is_left};
 
 // returns index of left tangenet to point q
-fn left_tangent(n: u64, p: Vec<(f64, f64)>, q: (f64, f64)) -> u64 {
+fn left_tangent(n: u64, p: &[(f64, f64)], q: (f64, f64)) -> u64 {
+    // first point is the left tangent
+    if is_left(q, p[(n - 1) as usize], p[0]) && !is_left(q, p[0], p[1]) {
+        0
+    } else {
+        let mut i: u64 = 1;
+        let mut offset: u64 = 1;
+        loop {
+            let prev = p[((i - 1) % n) as usize];
+            let current = p[i as usize];
+            let next = p[((i + 1) % n) as usize];
 
-    // test if first point is left tangent
-    println!(
-        "0 - {} && !{} => {}",
-        is_left(q, p[(n - 1) as usize], p[0]),
-        is_left(q, p[0], p[1]),
-        is_left(q, p[(n - 1) as usize], p[0]) && !is_left(q, p[0], p[1])
-    );
-    if is_right(q, p[0], p[1]) && is_left(q, p[(n - 1) as usize], p[0]) {
-        return 0;
+            let edge_dir = is_left(q, current, next);
+
+            // current point is the tangent
+            if is_left(q, prev, current) && !edge_dir {
+                return i;
+            }
+
+            let prev_edge_dir = is_left(q, p[(i - offset) as usize], p[(i - offset + 1) as usize]);
+            // current edge left
+            if edge_dir {
+                // prev edge left and current point right of prev point
+                if prev_edge_dir && !is_left(q, p[(i - offset) as usize], current) {
+                    i -= offset;
+                    offset = 1;
+                }
+                // previous edge is right  OR  current edge left of previous edge
+                else {
+                    offset *= 2;
+                }
+            }
+            // current edge is right
+            else {
+                // prev edge is left
+                if prev_edge_dir {
+                    i -= offset;
+                    offset = 1;
+                }
+                // prev edge is right
+                else {
+                    offset *= 2;
+                }
+            }
+
+            i = std::cmp::min(i + offset, n - 1);
+        }
     }
-
-    let a = 0;
-    let b = n;
-    for i in 1..(n - 1) {
-        let prev = p[(i - 1) as usize];
-        let current = p[i as usize];
-        let next = p[(i + 1) as usize];
-
-        println!(
-            "{} - {} && !{} => {}",
-            i,
-            is_left(q, prev, current),
-            is_left(q, current, next),
-            (is_left(q, prev, current) && !is_left(q, current, next))
-        );
-    }
-
-    (n - 1) as u64
-
 }
 
 fn main() {
     // q point
     let origin = (0.0, 0.0);
 
-    // Left tangent = (1, 3)
-    //let poly_small = Polygon {
-    //    n: 3,
-    //    points: vec![(1.0, 1.0), (3.0, 1.0), (1.0, 3.0)],
-    // };
-    //let mut result: (f64, f64) = left_tangent(poly_small, origin);
-    //let mut correct: (f64, f64) = (1.0, 3.0);
-    //assert!(result.0 - correct.0 <= 0.000000000001 && result.1 - correct.0 <= 0.000000000001);
+    // Left tangent = 2:(1, 3)
+    let poly_small = Polygon {
+        n: 3,
+        points: vec![(1.0, 1.0), (3.0, 1.0), (1.0, 3.0)],
+    };
+    let mut result: u64 = left_tangent(poly_small.n, &poly_small.points, origin);
+    assert!(poly_small.points[2] == poly_small.points[result as usize]);
 
     // Left tangent = 9:(1.25, 4.7)
     let poly_large = Polygon {
         n: 17,
         points: vec![
+            (6.10, 2.29),
             (6.46, 3.22),
             (6.46, 4.22),
             (6.10, 5.15),
@@ -69,12 +86,10 @@ fn main() {
             (3.53, 1.01),
             (4.53, 1.10),
             (5.43, 1.55),
-            (6.10, 2.29),
         ],
     };
-    let result: u64 = left_tangent(poly_large.n, poly_large.points, origin);
-    //let correct: (f64, f64) = (1.25, 4.70);
-    println!("({})", result);
-    //assert!(result.0 - correct.0 <= 0.000000000001 && result.1 - correct.0 <= 0.000000000001);
+    result = left_tangent(poly_large.n, &poly_large.points, origin);
+    assert!(poly_large.points[9] == poly_large.points[result as usize]);
 
+    println!("All tests passed!");
 }
